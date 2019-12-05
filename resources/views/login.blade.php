03f3fb12de6b2eff55f953a12767fa8890ac29d1
baha908 <baho908@gmail.com>
2019-12-05 04:04:30 +0300

    login screen

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,21 +10,30 @@
     <script src="js/app.js" charset="utf-8"></script>
   </head>
   <body class="text-center">
-    <form class="form-signin">
-      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
-    </form>
+
+    <div class="wrapper fadeInDown">
+    <div id="formContent">
+        <!-- Tabs Titles -->
+
+        <!-- Icon -->
+        <div class="fadeIn first">
+        <img src="img/user.svg" id="icon" alt="User Icon" />
+        </div>
+
+        <!-- Login Form -->
+        <form>
+        <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+        <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+        <input type="submit" class="fadeIn fourth" value="Log In">
+        </form>
+
+        <!-- Remind Passowrd -->
+        <div id="formFooter">
+        <a class="underlineHover" href="#">Forgot Password?</a>
+        </div>
+
+    </div>
+    </div>
   
 
 </body></html>
